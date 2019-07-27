3bf602d963d1e195b804e3c1953b5873ef0059d9
alber <alber@LAPTOP-TJ4NIO67>
2019-07-27 00:19:45 -0500

    Se creó la función setTimeout

--- src/vista/Main.java
+++ src/vista/Main.java
@@ -28,6 +28,19 @@ public class Main extends javax.swing.JFrame {
         //MANDA DESKTOPPANEL DEL MAIN A LA CLASE validaJFrameAbreUnaVez
         this.validajFraAbreUnaVez = new validaJframeAbreUnaVez(jDesktopMain);
         this.setExtendedState(MAXIMIZED_BOTH);
+//        setTimeout(() -> validajFraAbreUnaVez.abrirInternalFramePersonal(CatalogoEstilos.getInstancia()), 500);
+    }
+    
+    public static void setTimeout(Runnable runnable, int delay){
+        new Thread(() -> {
+            try {
+                Thread.sleep(delay);
+                runnable.run();
+            }
+            catch (Exception e){
+                System.err.println(e);
+            }
+        }).start();
     }
 
     /**
