812a5e4f01b8043d33ac36868200182472c5ffa9
alber <alber@LAPTOP-TJ4NIO67>
2019-07-27 00:23:28 -0500

    Se agregó el método llenaPanelProcesos

--- src/modelo/Procesos.java
+++ src/modelo/Procesos.java
@@ -11,7 +11,9 @@ import java.sql.ResultSetMetaData;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.JComboBox;
 import javax.swing.JOptionPane;
+import javax.swing.JRadioButton;
 import persistencia.BD;
+import vista.CatalogoEstilos;
 
 /**
  *
@@ -59,6 +61,31 @@ public class Procesos {
         } catch (Exception e) {
             System.out.println("Excepción: "+e);
             JOptionPane.showMessageDialog(null, "Error al mostrar el listado de procesos",
+                    "Error",JOptionPane.ERROR_MESSAGE);
+            BD.cerrarConexion();
+        }
+    }
+    
+    public void llenaPanelProcesos(){
+        try {
+            if (BD.conectarBD()) {
+                String sql = "select idProceso,nombre from procesos";
+                rs = BD.ejecutarSQLSelect(sql);
+                rsm = rs.getMetaData();
+                JRadioButton  radio = null;
+                while (rs.next()) { 
+                    radio = new JRadioButton(rs.getString("nombre"));
+                    CatalogoEstilos.panelProcesos.add(radio);
+                    CatalogoEstilos.radiosProcesos.add(radio);
+                    CatalogoEstilos.panelProcesos.updateUI();
+                }
+            } else {
+                JOptionPane.showMessageDialog(null, "Error al intentar conectar con la base de datos plantasbd",
+                        "Error de conexión",JOptionPane.ERROR_MESSAGE);
+                BD.cerrarConexion();
+            }            
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null,"Error al intentar llenar el panel de procesos en el Catálogo de Estilos: "+e, 
                     "Error",JOptionPane.ERROR_MESSAGE);
             BD.cerrarConexion();
         }
