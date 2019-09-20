487ee811719f1a92ebd26d58a2a5b951fa145880
alber <alber@LAPTOP-TJ4NIO67>
2019-09-19 22:37:23 -0500

    Actualización ResportePagoPorDestajo

--- src/modelo/ReporPagoPorDestajo.java
+++ src/modelo/ReporPagoPorDestajo.java
@@ -120,4 +120,37 @@ public class ReporPagoPorDestajo {
         }
     }
     
+    public void buscarEstilosPorPersonal(){
+        try {
+            if (BD.conectarBD()) {
+                String sql = "select estilos.idEstilo,estilos.estilo, estilos_procesos.proceso,sum(tallas.cantidad) as totalTallas, procesos.destajo,"+
+                        "round(sum(tallas.cantidad)*(procesos.destajo),2) as totalDes from pedidos inner join tallas on tallas.idPedido=pedidos.idPedido "+
+                        "inner join estilos on estilos.idEstilo=pedidos.idEstilo inner join estilos_procesos on estilos_procesos.idEstilo=estilos.idEstilo "+
+                        "inner join procesos on procesos.nombre=estilos_procesos.proceso WHERE pedidos.idPersonal =4 ";
+                rs = BD.ejecutarSQLSelect(sql);
+                rsm = rs.getMetaData();
+                List<Object[]> datos = new ArrayList<Object[]>();
+                while (rs.next()) {                
+                    Object[] filas = new Object[rsm.getColumnCount()];
+                    for (int i = 0; i < filas.length; i++) {
+                        filas[i] = rs.getObject(i+1);
+                    }
+                    datos.add(filas);
+                }
+                dtm = (DefaultTableModel)PagosPorDestajo.jTablePrincipalPagosDestajo.getModel();
+                for (int i = 0; i < datos.size(); i++) {
+                    dtm.addRow(datos.get(i));
+                }
+            } else {
+                JOptionPane.showMessageDialog(null, "Error al intentar conectar con la base de datos plantasbd",
+                        "Error de conexión",JOptionPane.ERROR_MESSAGE);
+                BD.cerrarConexion();
+            }            
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null,"Error al mostrar lo datos PagosPorDestajo-tablaPrincipal "+e, 
+                    "Error",JOptionPane.ERROR_MESSAGE);
+            BD.cerrarConexion();
+        }
+    }
+    
 }
